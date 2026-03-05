fa1427395cfe4bf75f19ad718cdd59b86bc5f323
Arjun <aa6709@srmist.edu.in>
2026-03-05 13:31:03 +0530

    UC12

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,37 @@
 import java.util.*;
-class Uc11 {
-    public void check() {
-        String input = "refer";
-        int start = 0;
-        int end = input.length() - 1;
-        boolean palin = true;
-        while (start < end) {
-            if (input.charAt(start) != input.charAt(end)) {
-                palin = false;
-                break;
+
+interface PalindromeStrategy {
+    void check();
+}
+class Uc11 implements PalindromeStrategy {
+        public void check() {
+            boolean palin=true;
+            String input="refer";
+            Queue<Character> q = new LinkedList<>();
+            Stack<Character> s = new Stack<>();
+
+            for (int i = 0; i < input.length(); i++) {
+                q.add(input.charAt(i));
+                s.push(input.charAt(i));
             }
-            start++;
-            end--;
+
+            for (int i = 0; i < input.length() / 2; i++) {
+                if (q.remove() != s.pop()) {
+                    palin = false;
+                    break;
+                }
+            }
+            if(palin) {
+                System.out.println("palindrome" +input);
+            }
+            else{
+                System.out.println("not palindrome");
+            }
         }
-        if (palin) {
-            System.out.println("Palindrome verified: " + input);
-        } else {
-            System.out.println("Not a Palindrome: " + input);
-        }
-    }
 }
 public class Main {
     public static void main(String[] args) {
-        Uc11 ob=new Uc11();
+        PalindromeStrategy ob=new Uc11();
         ob.check();
     }
 }
