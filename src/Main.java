7f12776bde14d00802c9045e82cde9e954cfa435
arjun <aa6709@srmist.edu.in>
2026-03-05 08:48:20 +0530

    UC13

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 import java.util.*;
-class Uc11 {
+class Uc13 {
     public void check() {
         String input = "refer";
+        long startTime = System.nanoTime();
         int start = 0;
         int end = input.length() - 1;
         boolean palin = true;
@@ -13,16 +14,18 @@ class Uc11 {
             start++;
             end--;
         }
+        long endTime = System.nanoTime();
         if (palin) {
             System.out.println("Palindrome verified: " + input);
         } else {
             System.out.println("Not a Palindrome: " + input);
         }
+        System.out.println("Execution Time: " + (endTime - startTime) + " ns");
     }
 }
 public class Main {
     public static void main(String[] args) {
-        Uc11 ob=new Uc11();
+        Uc13 ob=new Uc13();
         ob.check();
     }
 }
